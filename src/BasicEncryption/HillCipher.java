1ac6dcd838b669995be3475975d88bd64d585955
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-03-27 22:48:12 +0700

    fix match pattern

--- src/BasicEncryption/HillCipher.java
+++ src/BasicEncryption/HillCipher.java
@@ -3,7 +3,7 @@ package BasicEncryption;
 public class HillCipher {
     public static void main(String[] args) {
         int[][] key = {{3,3},{2,5}};
-        System.out.println(encrypt("bcd", key));
+        System.out.println(encrypt("Ủy viên Ban Chấp hành Trung ương Đảng khóa XI, Ủy viên Ban Thường vụ Đảng ủy Công an Trung ương, Thứ trưởng Bộ Công an; tháng 9/2014 thăng cấp bậc hàm Thượng tướng", key));
     }
     public static int[] nhanMatrix(int[][] key, int[]vector){
         int[] res = new int[2];
@@ -13,7 +13,7 @@ public class HillCipher {
     }
     public static String encrypt(String text, int[][] k){
         StringBuilder builder = new StringBuilder();
-        text = text.toUpperCase();
+        text = text.toUpperCase().replaceAll("[^A-Za-z]", "");
         if(text.length() % 2 != 0){
             text += 'X';
         }
