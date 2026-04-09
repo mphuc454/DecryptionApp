437cfd1e6e2d15aafc53d73325365df9441c27df
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-04-09 21:29:03 +0700

    them aes vao

--- src/SymmetricEncoding/AES.java
+++ src/SymmetricEncoding/AES.java
@@ -4,16 +4,17 @@ import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
-public class DES {
+public class AES {
     private SecretKey key;
     IvParameterSpec iv;
-    public SecretKey createKey() throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
-        keyGenerator.init(56);
+    public SecretKey genKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
         key = keyGenerator.generateKey();
         return key;
     }
@@ -24,21 +25,21 @@ public class DES {
     public void loadKey(SecretKey key){
         this.key = key;
     }
-    public byte[] encypt(String txt) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.ENCRYPT_MODE, this.key);
+    public byte[] encrypt(String txt) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, this.key, iv);
         byte[] data = txt.getBytes(StandardCharsets.UTF_8);
         return cipher.doFinal(data);
     }
-    public String decrypt(byte[] data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher =  Cipher.getInstance("DES");
-        cipher.init(Cipher.DECRYPT_MODE, this.key);
+    public String decrypt(byte[] data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        Cipher cipher =  Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, this.key, iv);
         byte[] bytes = cipher.doFinal(data);
         return new String(bytes, StandardCharsets.UTF_8);
     }
-    public boolean encryptFile(String src, String des) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.ENCRYPT_MODE, this.key);
+    public boolean encryptFile(String src, String des) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, this.key, iv);
         BufferedInputStream input = new BufferedInputStream(new FileInputStream(src));
         BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(des));
         CipherInputStream in = new CipherInputStream(input, cipher);
@@ -57,9 +58,9 @@ public class DES {
         output.close();
         return true;
     }
-    public boolean decryptFile(String src, String des) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher = Cipher.getInstance("DES");
-        cipher.init(Cipher.ENCRYPT_MODE, this.key);
+    public boolean decryptFile(String src, String des) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, this.key, iv);
         BufferedInputStream input = new BufferedInputStream(new FileInputStream(src));
         BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(des));
         CipherOutputStream out = new CipherOutputStream(output, cipher);
@@ -79,12 +80,15 @@ public class DES {
         return true;
     }
 
-    public static void main(String[] args) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
-        String text = "cầu thủ của người ta . Gáy sao cũng chấp nhận vì đó là sự thật hiển nhiên ";
-        DES des = new DES();
-        des.createKey();
-        byte[] re = des.encypt(text);
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        String text = " . Gáy sao cũng chấp nhận vì đó là sự thật hiển nhiên ";
+        AES aes = new AES();
+        aes.genKey();
+        aes.genIV();
+        byte[] re = aes.encrypt(text);
         System.out.println(Base64.getEncoder().encodeToString(re));
-        System.out.println(des.decrypt(re));
+        System.out.println(aes.decrypt(re));
+
     }
+
 }
