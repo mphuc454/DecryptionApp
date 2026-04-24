9c517f143f2066ec580ea3eef9adc3224bab6967
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-04-24 11:22:29 +0700

    fix giao dien swing

--- src/GUI/View/MyApp.java
+++ src/GUI/View/MyApp.java
@@ -4,5 +4,6 @@ public class MyApp {
     public static void main(String[] args) {
         MyView view = new MyView();
         view.setVisible(true);
+
     }
 }
--- src/GUI/View/MyView.java
+++ src/GUI/View/MyView.java
@@ -4,30 +4,75 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyView extends JFrame {
-    JPanel panelTitle1 = new JPanel();
-    JPanel panelTitle2 = new JPanel();
-    JPanel panelButton1 = new JPanel();
-    JLabel label1 = new JLabel("Mã hoá truyền thống");
+    JPanel panelMain = new JPanel();
+    JPanel panelTraditional = new JPanel();
+    JPanel panelSymmetric = new JPanel();
+    JPanel panelAsymmetric = new JPanel();
+    JPanel panelHash = new JPanel();
+    final int maxWidth = Integer.MAX_VALUE;
+    final int maxHeight = 300;
 
 
 
     public MyView(){
         this.setTitle("App Encryption Basic");
-        this.setSize(700, 500);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
         this.setLayout(new BorderLayout());
-        panelTitle1.setBackground(Color.red);
-        panelTitle2.setBackground(Color.green);
-        panelTitle1.add(label1);
-        panelButton1.add(new JButton("AffineCipher"));
-        panelButton1.add(new JButton("CaesarCipher"));
-        panelButton1.add(new JButton("HillCipher"));
-        panelButton1.add(new JButton("Substitution"));
-        panelButton1.add(new JButton("VigenereCipher"));
-        panelTitle1.setPreferredSize(new Dimension(200, 50));
-        this.add(panelTitle1, BorderLayout.WEST);
-        this.add(panelTitle2, BorderLayout.WEST);
-        this.add(panelButton1, BorderLayout.CENTER);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panelMain.setLayout(new BoxLayout(panelMain, BoxLayout.Y_AXIS));
+        panelMain.add(panelTraditional);
+        panelMain.add(Box.createVerticalStrut(10));
+        panelMain.add(panelSymmetric);
+        panelMain.add(Box.createVerticalStrut(10));
+        panelMain.add(panelAsymmetric);
+        panelMain.add(Box.createVerticalStrut(10));
+        panelMain.add(panelHash);
+
+        panelTraditional.setLayout(new BoxLayout(panelTraditional, BoxLayout.X_AXIS));
+        panelSymmetric.setLayout(new BoxLayout(panelSymmetric, BoxLayout.X_AXIS));
+        panelAsymmetric.setLayout(new BoxLayout(panelAsymmetric, BoxLayout.X_AXIS));
+        panelHash.setLayout(new BoxLayout(panelHash, BoxLayout.X_AXIS));
+
+        panelTraditional.setBorder(BorderFactory.createTitledBorder("Mã hoá truyền thống"));
+        panelTraditional.setMaximumSize(new Dimension(maxWidth, maxHeight));
+        panelTraditional.add(new JButton("AffineCipher"));
+        panelTraditional.add(Box.createHorizontalStrut(10));
+        panelTraditional.add(new JButton("CaesarCipher"));
+        panelTraditional.add(Box.createHorizontalStrut(10));
+        panelTraditional.add(new JButton("HillCipher"));
+        panelTraditional.add(Box.createHorizontalStrut(10));
+        panelTraditional.add(new JButton("Substitution"));
+        panelTraditional.add(Box.createHorizontalStrut(10));
+        panelTraditional.add(new JButton("VigenereCipher"));
+
+        panelSymmetric.setBorder(BorderFactory.createTitledBorder("Mã hoá Đối xứng"));
+        panelSymmetric.setMaximumSize(new Dimension(maxWidth, maxHeight));
+        panelSymmetric.add(new JButton("AES"));
+        panelSymmetric.add(Box.createHorizontalStrut(10));
+        panelSymmetric.add(new JButton("DES"));
+
+        panelAsymmetric.setBorder(BorderFactory.createTitledBorder("Mã hoá Bất Đối xứng"));
+        panelAsymmetric.setMaximumSize(new Dimension(maxWidth, maxHeight));
+        panelAsymmetric.add(new JButton("RSA"));
+
+        panelHash.setBorder(BorderFactory.createTitledBorder("Hàm băm"));
+        panelHash.setMaximumSize(new Dimension(maxWidth, maxHeight));
+        panelHash.add(new JButton("SHA"));
+        panelHash.add(Box.createHorizontalStrut(10));
+        panelHash.add(new JButton("MD5"));
+
+
+
+        this.add(panelMain, BorderLayout.CENTER);
+
+
+
+
+
+
+
+
 
     }
 
